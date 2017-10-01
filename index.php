c6756f48b0e6b1f932f26e707d869c1751f9d115
jamesread <contact@jwread.com>
2017-10-01 21:54:05 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once 'lanCacheStatusFunctions.php';
+
+echo '<h1>Lan Cache Status</h1>';
+echo getStatus();
+
+?>
---
+++ lanCacheStatusFunctions.php
@@ -0,0 +1,7 @@
+<?php
+
+function getStatus() {
+	echo "OK";
+}
+
+?>
