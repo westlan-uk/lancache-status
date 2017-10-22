7540f0fb101bd58111670a2cbd27d52bc28aa4af
root <root@lancache.westlan.lan>
2017-10-22 13:11:45 +0100

    Some updates from SL43

--- index.php
+++ index.php
@@ -3,6 +3,18 @@
 require_once 'lanCacheStatusFunctions.php';
 
 echo '<h1>Lan Cache Status</h1>';
-echo getStatus();
+echo '<link rel = "stylesheet" href = "style.css" />';
+
+echo '<table>';
+foreach (getStatus() as $stat) {
+	if (empty($stat['description'])) {
+		$stat['description'] = null;
+	}
+
+	echo '<tr>';
+	echo '<td>' . $stat['name'] . '</td><td>' . $stat['value'] . '</td><td>' . $stat['description']. '</td>';
+	echo '</tr>';
+}
+echo '</table>';
 
 ?>
--- lanCacheStatusFunctions.php
+++ lanCacheStatusFunctions.php
@@ -1,7 +1,54 @@
 <?php
 
+function bytesToHuman($bytes) {
+	$si_prefix = array('B', 'KB', 'MB', 'GB', 'TB');
+	$base = 1024;
+
+	$class = min((int)log($bytes, $base), count($si_prefix) -1);
+
+	return number_format($bytes / pow($base, $class), 2) . $si_prefix[$class];
+}
+
+function isProcessRunning($prog) {
+	exec("pgrep $prog", $pids);
+
+	if (empty($pids)) {
+		return 'false';
+	} else {
+		return 'true';
+	}
+}
+
 function getStatus() {
-	echo "OK";
+	return array(
+		array( 
+			'name' => 'disk_free_space',
+			'value' => bytesToHuman(disk_free_space("/srv/lancache")),
+		),
+		array( 
+			'name' => 'requests_HIT',
+			'value' => number_format(trim(`grep HIT /srv/lancache/logs/Keys/steam.log | wc -l `)),
+			'description' =>  'Number of requests that we served from the cache',
+		),
+		array(
+			'name' => 'requests_MISS',
+			'value' => number_format(trim(`grep MISS /srv/lancache/logs/Keys/steam.log | wc -l `)),
+			'description' =>  'Number of requests that we went to the internet for'
+		),
+		array(
+			'name' => 'sniproxy_running',
+			'value' => isProcessRunning('sniproxy'),
+		),
+		array( 
+			'name' => 'nginx_running',
+			'value' => isProcessRunning('nginx'),
+		),
+		array( 
+			'name' => 'disk_used_space',
+			'value' => nl2br(`tac disk_used `),
+		),
+
+	);
 }
 
 ?>
